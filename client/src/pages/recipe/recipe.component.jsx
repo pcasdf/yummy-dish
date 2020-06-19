dc6466321031c2e90c3c6268602c262da32e6886
Peter Cho <pcho51990@gmail.com>
2020-06-19 18:09:04 -0400

    pushing

--- client/src/pages/recipe/recipe.component.jsx
+++ client/src/pages/recipe/recipe.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ReactComponent as Icon } from '../../assets/star.svg';
@@ -7,6 +7,7 @@ import './recipe.styles.scss';
 
 const RecipeDetail = () => {
   const { id } = useParams();
+  const [tab, setTab] = useState(0);
 
   const recipe = details.find((item) => item.id === +id);
 
@@ -18,51 +19,78 @@ const RecipeDetail = () => {
     extendedIngredients
   } = recipe;
 
-  console.log(recipe);
+  const Recipe = () => (
+    <div className='info'>
+      <span className='title'>{title}</span>
+      <div className='duration'>
+        <span>10 MIN</span>
+        <span>Easy</span>
+        <Icon className='icon' />
+        <span>$$$</span>
+      </div>
+      <div className='instructions'>
+        <div>
+          <span>YOU WILL NEED:</span>
+          <ul className='ingredient-list'>
+            {extendedIngredients.map(({ original }, idx) => (
+              <li key={idx}>{original}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <span>DIRECTIONS:</span>
+          <ol>
+            {analyzedInstructions[0].steps.map(({ step }, idx) => (
+              <li key={idx}>{step}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
+
+  const Story = () => (
+    <div className='story'>
+      <span className='label'>STORY</span>
+      <div className='summary' dangerouslySetInnerHTML={{ __html: summary }} />
+    </div>
+  );
+
+  const Reviews = () => (
+    <div className='reviews'>
+      <span className='label'>REVIEWS</span>
+      <div className='ratings'></div>
+    </div>
+  );
+
+  let body;
+
+  if (tab === 0) {
+    body = (
+      <>
+        <div className='body'>
+          <Recipe />
+          <Story />
+        </div>
+      </>
+    );
+  } else if (tab === 1) {
+    body = <Recipe />;
+  } else if (tab === 2) {
+    body = <Story />;
+  } else {
+    body = <Reviews />;
+  }
 
   return (
     <div className='recipe-detail'>
       <div className='header' style={{ backgroundImage: `url(${image})` }} />
-      <div className='body'>
-        <div className='info'>
-          <span className='title'>{title}</span>
-          <div className='duration'>
-            <span>10 MIN</span>
-            <span>Easy</span>
-            <Icon className='icon' />
-            <span>$$$</span>
-          </div>
-          <div className='instructions'>
-            <div>
-              <span>YOU WILL NEED:</span>
-              <ul className='ingredient-list'>
-                {extendedIngredients.map(({ original }, idx) => (
-                  <li key={idx}>{original}</li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <span>DIRECTIONS:</span>
-              <ol>
-                {analyzedInstructions[0].steps.map(({ step }, idx) => (
-                  <li key={idx}>{step}</li>
-                ))}
-              </ol>
-            </div>
-          </div>
-        </div>
-        <div className='story'>
-          <span className='label'>STORY</span>
-          <div
-            className='summary'
-            dangerouslySetInnerHTML={{ __html: summary }}
-          />
-        </div>
+      <div className='tabs'>
+        <span onClick={() => setTab(1)}>RECIPE</span>
+        <span onClick={() => setTab(2)}>STORY</span>
+        <span onClick={() => setTab(3)}>REVIEW</span>
       </div>
-      <div className='reviews'>
-        <span className='label'>REVIEWS</span>
-        <div className='ratings'></div>
-      </div>
+      <div>{body}</div>
     </div>
   );
 };
