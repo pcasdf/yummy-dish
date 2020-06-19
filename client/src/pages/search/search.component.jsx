7d64811286504de3a6a1f7bf3e6a4bd2c3ef3429
Estevan Acevedo <estevanacevedo@Estevans-MacBook-Pro.local>
2020-06-19 15:14:27 -0700

    searchbarbasics

---
+++ client/src/pages/search/search.component.jsx
@@ -0,0 +1,82 @@
+
+
+import React, { useState, useEffect } from "react";
+import detailsJSON from "./data/details-1.json"
+import recipesJSON from "./data/recipes-1.json"
+import TagsInput from './TagsInput';
+
+
+function Search() {
+  const [tags, setTags] = useState([])
+  // const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  // const handleChange = (e) => {
+  //   setSearchTerm(e.target.value)
+  // }
+
+  
+    const details = detailsJSON
+    const recipes = recipesJSON
+  console.log(details)
+  console.log(recipes)
+
+
+  
+  // useEffect(() => {
+  //   const results = details.filter(detail =>
+  //     detail.title.toLowerCase().includes(tags)
+  //   );
+  //   setSearchResults(results);
+  // }, [tags]);
+
+  useEffect(() => {
+    if (tags.length) {
+      let next = [];
+      details.forEach(item => {
+        if (
+          tags.reduce(
+            (acc, curr) => acc && item.title.toLowerCase().includes(curr),
+            true
+          )
+        ) {
+          next.push(item);
+        }
+      });
+      setSearchResults(next);
+    } else {
+      setSearchResults(details);
+    }
+}, [tags]);
+
+  return (
+    
+    <div className="Search">
+      <TagsInput 
+        
+        // value={searchTerm}
+        // onChange={handleChange}
+        tags={tags}
+        setTags={setTags}
+     
+      
+      
+      />
+     
+      <ul>
+        {searchResults.map(item => (
+           <>
+          <div>{item.title}</div>
+            <img src={item.image} />
+            </>
+           
+           
+        ))}
+        
+      </ul>
+      
+      </div>
+      
+  );
+}
+
+export default Search;
