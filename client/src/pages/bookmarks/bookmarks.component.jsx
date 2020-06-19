fd0967eb16328231cb279756dd841138b9600193
Kate Fang <Kate@MacBook-Pro.local>
2020-06-19 15:09:34 -0400

    update header

--- client/src/pages/bookmarks/bookmarks.component.jsx
+++ client/src/pages/bookmarks/bookmarks.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Tabs from '../../components/tabs/tabs.component';
+import Header from '../../components/header/header.component';
 
 const Bookmarks = () => {
   return (
     <>
+      <Header>My Recipes</Header>
       <Tabs>Snacks</Tabs>
       <Tabs>Desserts</Tabs>
       <Tabs>For Party</Tabs>
