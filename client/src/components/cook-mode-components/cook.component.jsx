2fc42382f568ac722dda0a5d8f777c3c6ca1f106
Kate Fang <Kate@MacBook-Pro.local>
2020-06-23 18:46:25 -0400

    add create review functionality and style for review page

--- client/src/components/cook-mode-components/cook.component.jsx
+++ client/src/components/cook-mode-components/cook.component.jsx
@@ -5,7 +5,6 @@ import './cook.styles.scss';
 
 const Cook = ({ id }) => {
   const recipe = Data.find((each) => each.id === +id);
-  console.log(recipe.analyzedInstructions[0].steps);
   return (
     <div className='cook-container'>
       <div className='content-container'>
@@ -14,18 +13,27 @@ const Cook = ({ id }) => {
           MAKE IT YOUR OWN! <Edit className='edit-icon' />
         </h3>
         <h2>DIRECTIONS:</h2>
-        {recipe.analyzedInstructions[0].steps.map((each) => (
-          <div className='directions'>
-            <div className='input-label'>
-              <input type='checkbox' className='cook-steps-checkbox' />
-              <label htmlFor='' className='cook-steps-label'>
-                <span>
-                  {each.number}. {each.step}
-                </span>
-              </label>
+        {recipe.analyzedInstructions[0] ? (
+          recipe.analyzedInstructions[0].steps.map((each) => (
+            <div className='directions'>
+              <div className='input-label'>
+                <input type='checkbox' className='cook-steps-checkbox' />
+                <label className='cook-steps-label'>
+                  <span>
+                    {each.number}. {each.step}
+                  </span>
+                </label>
+              </div>
             </div>
+          ))
+        ) : (
+          <div className='input-label'>
+            <input type='checkbox' className='cook-steps-checkbox' />
+            <label className='cook-steps-label'>
+              Just Mix Everything Together.
+            </label>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
--- client/src/components/cook-mode-components/reviews.component.jsx
+++ client/src/components/cook-mode-components/reviews.component.jsx
@@ -1,21 +1,60 @@
 import React, { useState, useContext } from 'react';
 import { Rating } from '@material-ui/lab/';
 import { Star } from '@material-ui/icons';
+import { Box } from '@material-ui/core';
 import { ReactComponent as Heart } from '../../assets/heart.svg';
 import Data from '../../data/details-1.json';
 import './reviews.styles.scss';
 import { createReview } from '../../services/reviews';
 import { UserContext } from '../../contexts/user.context';
+import { useParams } from 'react-router-dom';
 
-const Reviews = ({ id }) => {
-  const recipe = Data.find((each) => each.id === +id);
+const Reviews = () => {
+  const { id } = useParams();
   const { user } = useContext(UserContext);
-  const [input, setInput] = useState({
-    review: ''
-  });
+  const [review, setReview] = useState('');
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(-1);
 
-  const handleClick = (e) => {
+  const labels = {
+    0.5: '💩',
+    1: '🤮',
+    1.5: '🤢',
+    2: '🤬',
+    2.5: '👎',
+    3: '😐',
+    3.5: '👌',
+    4: '👍',
+    4.5: '😋',
+    5: '😍'
+  };
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setReview(value);
+  };
+
+  const handleRatingChange = (e, newRating) => {
+    setRating(newRating);
+  };
+
+  const handleHoverChange = (e, newHover) => {
+    setHover(newHover);
+  };
+
+  const handleClick = async (e) => {
     e.preventDefault();
+    try {
+      const createdReview = await createReview({
+        recipe: +id,
+        rating,
+        user: user._id,
+        comment: review
+      });
+      console.log(createdReview);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className='reviews-container'>
@@ -24,14 +63,26 @@ const Reviews = ({ id }) => {
         <p>How did it go?</p>
         <div rating-bar>
           <Rating
+            size='medium'
+            onChange={handleRatingChange}
+            onChangeActive={handleHoverChange}
             className={Rating}
             name='half-rating'
             defaultValue={0}
             precision={0.5}
             emptyIcon={<Star style={{ color: '#fff' }} />}
           />
+          {rating !== null && (
+            <Box ml={2}>{labels[hover !== -1 ? hover : rating]}</Box>
+          )}
         </div>
-        <textarea placeholder='Write A Review...' rows='2' />
+        <textarea
+          name='review-input'
+          value={review}
+          onChange={handleInputChange}
+          placeholder='Write A Review...'
+          rows='2'
+        />
         <button onClick={handleClick} className='add-review-button'>
           SUBMIT REVIEW
         </button>
