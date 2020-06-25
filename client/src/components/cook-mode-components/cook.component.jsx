a2195c11e4c0baf1a67e0d10b78730f159da0b83
Kate Fang <Kate@MacBook-Pro.local>
2020-06-25 13:59:05 -0400

    working on substitutions

--- client/src/components/cook-mode-components/cook.component.jsx
+++ client/src/components/cook-mode-components/cook.component.jsx
@@ -6,9 +6,10 @@ import { FormControlLabel, Checkbox } from '@material-ui/core';
 const Cook = ({ id }) => {
   const recipe = Data.find((each) => each.id === +id);
   const customSteps = [
-    '1. Chop everything up.',
-    '2. Cook them however you want.',
-    '3. Hope for the best.'
+    'Chop everything up.',
+    'Cook them however you want.',
+    'Hope for the best.',
+    'Enjoy?'
   ];
   return (
     <div className='cook-container'>
@@ -18,42 +19,47 @@ const Cook = ({ id }) => {
           MAKE IT YOUR OWN! <Edit className='edit-icon' />
         </h3>
         <h2>DIRECTIONS:</h2>
-        {recipe.analyzedInstructions[0] ? (
-          recipe.analyzedInstructions[0].steps.map((each) => (
-            <div key={each.number} className='directions'>
-              <div className='input-label'>
-                <label className='cook-steps-label'>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        fontSize='small'
-                        color='primary'
-                        style={{ padding: 0 }}
-                      />
-                    }
-                  />
-                  <span className='cook-steps'>
-                    {each.number + '.'} {each.step}
-                  </span>
-                </label>
+        {recipe.analyzedInstructions[0]
+          ? recipe.analyzedInstructions[0].steps.map((each) => (
+              <div key={each.number} className='directions'>
+                <div className='input-label'>
+                  <label className='cook-steps-label'>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          fontSize='small'
+                          color='primary'
+                          style={{ padding: 0 }}
+                        />
+                      }
+                    />
+                    <span className='cook-steps'>
+                      {each.number + '.'} {each.step}
+                    </span>
+                  </label>
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <div className='directions'>
-            {customSteps.map((item, idx) => (
+            ))
+          : customSteps.map((item, idx) => (
               <div key={idx} className='directions'>
                 <div className='input-label'>
-                  <FormControlLabel
-                    control={<Checkbox color='primary' />}
-                    style={{ padding: 0 }}
-                  />
-                  <span className='cook-steps'>{item}</span>
+                  <label className='cook-steps-label'>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          fontSize='small'
+                          color='primary'
+                          style={{ padding: 0 }}
+                        />
+                      }
+                    />
+                    <span className='cook-steps'>
+                      {idx + 1 + '.'} {item}
+                    </span>
+                  </label>
                 </div>
               </div>
             ))}
-          </div>
-        )}
       </div>
     </div>
   );
