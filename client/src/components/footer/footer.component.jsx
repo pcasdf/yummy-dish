de04898bb2f273616672a91230bd397fb2dc9d79
Peter Cho <pcho51990@gmail.com>
2020-06-25 17:34:54 -0400

    theme context starting

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import './footer.styles.scss';
+import { ThemeContext } from '../../contexts/theme.context';
 
 const Footer = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    if (theme.header === 'black') {
+      setTheme({
+        header: '#fec368',
+        background: '#cbf3f0'
+      });
+    } else {
+      setTheme({
+        header: 'black',
+        background: '#444'
+      });
+    }
+  };
+
   return (
     <footer>
-      <span>About</span>
+      <span onClick={toggleTheme}>About</span>
       <span>Our Business</span>
       <span>Discover</span>
       <span>Languages</span>
--- client/src/pages/recipe/recipe.component.jsx
+++ client/src/pages/recipe/recipe.component.jsx
@@ -209,7 +209,7 @@ const RecipeDetail = () => {
               STORY
             </span>
             <span style={{ backgroundColor: tab3 }} onClick={() => setTab(3)}>
-              REVIEW
+              REVIEWS
             </span>
           </div>
         </div>
